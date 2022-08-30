0b17819abd69002ba0d2c9d8a98f2746c618630d
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2022-08-30 12:48:43 +0000

    upd

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -19,7 +19,7 @@ const PLATFORM_MAP = {
 export const Browsers = {
 	ubuntu: browser => ['Ubuntu', browser, '20.0.04'] as [string, string, string],
 	macOS: browser => ['Mac OS', browser, '10.15.7'] as [string, string, string],
-	baileys: browser => ['Baileys', browser, '4.0.0'] as [string, string, string],
+	baileys: browser => ['WhatsKuy', browser, '4.0.0'] as [string, string, string],
 	/** The appropriate browser based on your OS & release */
 	appropriate: browser => [ PLATFORM_MAP[platform()] || 'Ubuntu', browser, release() ] as [string, string, string]
 }
